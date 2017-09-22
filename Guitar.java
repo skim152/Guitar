d05c5b1dddf87294c0554746bd447194f16c6380
Justin S. Kim <skim152@student.umuc.edu>
2017-09-21 23:06:17 -0400

    Update Guitar.java

--- Guitar.java
+++ Guitar.java
@@ -1,3 +1,19 @@
+/*
+  Author: Shinyeob Kim
+  What does this program do? :
+  
+  A private int data field named numStrings that defines the number of strings on the guitar. The default value should be 6.
+  A private double data field named guitarLength that defines the length of the guitar in inches. The default value should be 28.2
+  A private String data field named guitarManufacturer that defines the manufacturer of the guitar. The default value should be “Gibson”.
+  A private Color data field named guitarColor that defines the color of the guitar. The default value should be Color.Red.
+  A no argument constructor that creates a Guitar using the default number of strings, length, manufacturer and color.
+  A constructor that creates a Guitar using a specified number of strings, length, manufacturer and color.
+  Getter methods for all data fields.
+  A playGuitar() method that returns a string representation of 16 randomly selected
+musical notes of random duration. For example, the first part of the string returned might look like this: [A(2), G(3), B(0.5), C(1), C(1), D(0.25), ...]. You can assume one octave in the key of C where valid notes include A, B, C, D, E, F and G and duration values are .25, .5, 1, 2, and 4 representing sixteenth notes, eighth notes, quarter notes, half notes and whole notes, respectively.
+  A toString() method that displays the number of strings, length, manufacturer and color in String format
+*/
+
 
 import  java.awt.* ;
 import java.util.Random;
